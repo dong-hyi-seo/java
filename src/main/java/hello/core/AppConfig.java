61f1e86fed910ec0cf5e2983f0f5660e463f440e
dong-hyi-seo <seodh1217@gmail.com>
2022-04-27 23:30:21 +0900

    1. 스프링 전환 진행중

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,6 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
@@ -9,6 +8,8 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * 기획자!!!
@@ -16,21 +17,31 @@ import hello.core.order.OrderServiceImpl;
  * 기획자가 memberService, orderService 에게 주입해주는데 해당 두개 입장에서는 외부에서 주입해주는 것같이 느낄것이다.. 이를!!
  * DI(Dependency Injection) 우리말로 의존관계 주입 또느 의존성 주입이라고 한다.
  */
+@Configuration
 public class AppConfig {
+
+    /**
+     * @Bean??
+     */
 
     //memberRepository 를 함수로 따로빼주면 해당 repository를 memory -> storage로 갈경우 한 함수내에서만 변경하면 됨!! (이점)
     //어플리케이션 전체 구성이 어떻게 되어있는지 빠르게 파악이가능 !!
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
+
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(new MemoryMemberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         //return new FixDiscountPolicy(); //쉽게 정액 할인정책, 정률할인정책을 이 함수하나에서만 변경하면됨..
         return new RateDiscountPolicy();
--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -6,13 +6,19 @@ import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig(); //기힉자 (DI)가 주입해준다!!!!
+        /*AppConfig appConfig = new AppConfig(); //기힉자 (DI)가 주입해준다!!!!
         MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        OrderService orderService = appConfig.orderService();*/
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
