92e68c622a05e4c1a8641705ee1b5c300fd19132
dong-hyi-seo <seodh1217@gmail.com>
2022-04-28 20:11:20 +0900

    1. 진행중

---
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,34 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스플이 없ㅇ는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+
+        //1조회 : 호출할때 마다 객체 생성
+        MemberService memberService = appConfig.memberService();
+
+        //2조회 : 호출할떄마다 객체 생서
+        MemberService memberService1 = appConfig.memberService();
+
+        //3조회 : 호출할떄마다 객체 생서
+        MemberService memberService2 = appConfig.memberService();
+
+        //참조값이 다른것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        //memberService1 != memberService2
+        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+
+    }
+}
