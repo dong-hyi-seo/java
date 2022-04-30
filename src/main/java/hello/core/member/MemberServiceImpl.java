9d5063b126ac346bc52a7dd284c490da31d92b98
dong-hyi-seo <seodh1217@gmail.com>
2022-04-30 15:57:04 +0900

    1. componentscan 진행

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,15 +1,21 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 /**
  * DIP (의존성 역전 법칙) 위배
  * MemberServiceImpl은 MemberRepository와 MemoryMemberRepository 둘다 의존하고있다.
  * 추상화에도 의존하고 구체화에도 의존한다.
  */
+@Component
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 
     //해당 부분은 AppConfig(기획자) 내가 할당해줄게 !! 이런뜻...
+    //@Autowired가 자동으로 MemberRMepository를 주입해준다.
+    @Autowired // = ac.getBean(MemberRepository.class) 와 같다
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class MemoryMemberRepository implements MemberRepository {
 
     //thread safe 하게 하려면 ConcurrentHashMap 사용
