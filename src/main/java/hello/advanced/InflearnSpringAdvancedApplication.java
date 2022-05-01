ff9a25f72a1880c849ea8e7d5654983eaed9c8ae
dong-hyi-seo <seodh1217@gmail.com>
2022-05-01 21:02:29 +0900

    commit

---
+++ src/main/java/hello/advanced/InflearnSpringAdvancedApplication.java
@@ -0,0 +1,13 @@
+package hello.advanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringAdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnSpringAdvancedApplication.class, args);
+	}
+
+}
