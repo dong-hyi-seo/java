1c2a4a44856451abb142c456b698eac5a6455d85
dong-hyi-seo <seodh1217@gmail.com>
2022-05-12 22:57:55 +0900

    1.콜백패턴 구현완료

--- src/test/java/hello/advanced/trace/strategy/code/strategy/ContextV2.java
+++ src/test/java/hello/advanced/trace/strategy/code/strategy/ContextV2.java
@@ -1,22 +1,14 @@
-package hello.advanced.trace.strategy.code;
+package hello.advanced.trace.strategy.code.strategy;
 
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * 전략패턴의 context v1 모델
- * 필드에 전략을 보관하는 방식
- * 변하지 않는 로직을 가지고있음 !! 템플릿역할
+ * 전략패턴 기존 ContextV2와 달리 excute에 파라미터로 전달받는다!
  */
 @Slf4j
-public class ContextV1 {
+public class ContextV2 {
 
-    private Strategy strategy;
-
-    public ContextV1(Strategy strategy) {
-        this.strategy = strategy;
-    }
-
-    public void execute() {
+    public void execute(Strategy strategy) {
         long startTime = System.currentTimeMillis();
         //비즈니스 로직 실행
         strategy.call();
--- src/test/java/hello/advanced/trace/strategy/code/template/TimeLogTemplate.java
+++ src/test/java/hello/advanced/trace/strategy/code/template/TimeLogTemplate.java
@@ -1,20 +1,19 @@
-package hello.advanced.trace.strategy.code;
+package hello.advanced.trace.strategy.code.template;
 
 import lombok.extern.slf4j.Slf4j;
 
-/**
- * 전략패턴 기존 ContextV2와 달리 excute에 파라미터로 전달받는다!
- */
 @Slf4j
-public class ContextV2 {
+public class TimeLogTemplate {
 
-    public void execute(Strategy strategy) {
+    public void execute(Callback callback) {
         long startTime = System.currentTimeMillis();
         //비즈니스 로직 실행
-        strategy.call();
+        callback.call();
         //비즈니스 로직종료
         long endTime = System.currentTimeMillis();
         long resultTime = endTime - startTime;
         log.info("resultTime = {}", resultTime);
     }
+
+
 }
