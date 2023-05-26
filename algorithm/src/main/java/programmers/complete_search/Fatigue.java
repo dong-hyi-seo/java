b0e4d4bf68faf05f1fb3ca107e15fad4d1b3b6fa
dong-hyi-seo <seodh1217@gmail.com>
2023-05-26 10:22:04 +0900

    1. 완전탐색 피로도관련 문제 DFS로 품

--- algorithm/src/main/java/programmers/complete_search/Fatigue.java
+++ algorithm/src/main/java/programmers/complete_search/Fatigue.java
@@ -1,7 +1,10 @@
 package programmers.complete_search;
 
+import java.util.Arrays;
+
 public class Fatigue {
 
+    private int answer = 0;
     /**
      * int k : 유저의 현재 피로도
      * int[][] dungeons : 최소필요피로도, 소모피로도
@@ -13,14 +16,27 @@ public class Fatigue {
      */
     public int solution(int k, int[][] dungeons) {
 
-        int answer = -1;
-
-        for (int i=0; i<dungeons.length; i++) {
-
-        }
-
+        int[] output = new int[dungeons.length];
+        boolean[] visited = new boolean[dungeons.length];
+        dfs(dungeons, visited, 0, k);
 
         return answer;
 
+    }
+
+    /**
+     *  유저의 현재 피로도가 K -> K가 최소피로도보다 작을때
+     */
+    private void dfs(int[][] dungeons, boolean[] visited, int depth, int K) {
+
+        for (int i=0; i<dungeons.length; i++) {
+            if (!visited[i] && dungeons[i][0] <= K) {
+                visited[i] = true;
+                dfs(dungeons, visited, depth+1, K - dungeons[i][1]);
+                visited[i] = false;
+            }
+        }
+        //결국 Depth값이 최대 많이 탐색한수이니까 최댓값찾으면됨
+        answer = Math.max(answer, depth);
     }
 }
