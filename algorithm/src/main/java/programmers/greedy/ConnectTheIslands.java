4572b41b0392e6eda7b13cc3eedca63e2fb09a6b
dong-hyi-seo <seodh1217@gmail.com>
2023-06-22 14:53:04 +0900

    1. 섬연결하기(크루스칼 알고리즘) 완료

--- algorithm/src/main/java/programmers/greedy/ConnectTheIslands.java
+++ algorithm/src/main/java/programmers/greedy/ConnectTheIslands.java
@@ -1,5 +1,7 @@
 package programmers.greedy;
 
+import java.util.Arrays;
+
 /**
  * 크루스칼 알고리즘 (최소신장트리)
  * 섬 연결하기 - Level3
@@ -8,6 +10,15 @@ package programmers.greedy;
  * n개의 섬 사이에 다리를 건설하는 비용(costs)이 주어질 때,
  * 최소의 비용으로 모든 섬이 서로 통행 가능하도록 만들 때 필요한
  * 최소 비용을 return 하도록 solution을 완성하세요.
+ *
+ * 섬의 개수 n은 1 이상 100 이하입니다.
+ * costs의 길이는 ((n-1) * n) / 2이하입니다.
+ * 임의의 i에 대해, costs[i][0] 와 costs[i] [1]에는 다리가 연결되는 두 섬의 번호가 들어있고,
+ * costs[i] [2]에는 이 두 섬을 연결하는 다리를 건설할 때 드는 비용입니다.
+ * 같은 연결은 두 번 주어지지 않습니다. 또한 순서가 바뀌더라도 같은 연결로 봅니다.
+ * 즉 0과 1 사이를 연결하는 비용이 주어졌을 때, 1과 0의 비용이 주어지지 않습니다.
+ * 모든 섬 사이의 다리 건설 비용이 주어지지 않습니다. 이 경우, 두 섬 사이의 건설이 불가능한 것으로 봅니다.
+ * 연결할 수 없는 섬은 주어지지 않습니다.
  *
  * 다리를 여러 번 건너더라도,
  * 도달할 수만 있으면 통행 가능하다고 봅니다.
@@ -27,9 +38,25 @@ public class ConnectTheIslands {
     public int solution(int n, int[][] costs) {
         int answer = 0;
 
-        //총 노드수를 구한다.
 
         //부모 상태를 담는 배열 초기화
+        int[] parent = new int[n+1];
+        for(int i=0; i<=n; i++) {
+            parent[i] = i;
+        }
+        //cost 순서대로 정렬
+        //간선기준으로 graph를 정렬한다 (오름차순)
+        Arrays.sort(costs, (o1, o2) -> o1[2] - o2[2]);
+
+        for(int[] node : costs) {
+            int a = node[0];
+            int b = node[1];
+            int cost = node[2];
+            if(find(parent, a) != find(parent, b)) {
+                union(parent, a, b);
+                answer += cost;
+            }
+        }
         return answer;
     }
 
--- algorithm/src/test/java/programmers/greedy/ConnectTheIslandsTest.java
+++ algorithm/src/test/java/programmers/greedy/ConnectTheIslandsTest.java
@@ -1,7 +1,7 @@
 package programmers.greedy;
 
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
